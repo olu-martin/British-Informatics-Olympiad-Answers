7acbd5a140a855b9e4130129b7c5eea1ca266916
olu-martin <76204016+olu-martin@users.noreply.github.com>
2020-12-18 09:18:31 +0000

    Update 2019 Q2.py

--- 2019 Q2.py
+++ 2019 Q2.py
@@ -13,7 +13,7 @@ class Explorer():
   def __init__(self,t ,i,m):
       self.t = t; self.i = i; self.m = m
       self.pos = [0,0]; self.direction = 0 #0,1,2,3 for N,E,S,W
-      self.dir = {0:[0,1], 1:[1,0], 2:[0,-1], 3:[-1,0]} #if direction is north increase y-coordinate by 1 e.t.c.
+      self.dir = {0:[0,1], 1:[1,0], 2:[0,-1], 3:[-1,0]} #if direction is north, increase y-coordinate by 1 e.t.c.
       self.trail = stack(t)
       self.trail.push(self.pos)
       
@@ -42,7 +42,7 @@ class Explorer():
           for i in self.i:
               if self.change_position(i):
                   move_count +=1
-                  '''print("{}:{},{}".format(move_count, self.pos[0],self.pos[1]))''' #Uncomment to see each step
+                  '''print("{}: {},{}".format(move_count, self.pos[0],self.pos[1]))''' #Uncomment to see each step
                   if move_count >= self.m: return self.pos
               else:
                   return self.pos
